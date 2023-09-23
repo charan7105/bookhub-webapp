58888adfb5ad5a66bcb06961bd63cae2ff8492bd
charan7105 <saicharanvenna123@gmail.com>
2023-09-23 14:35:49 +0530

    learn how to create page and adding styles

---
+++ nextfrontend/pages/about.js
@@ -0,0 +1,9 @@
+import styles from "../styles/About.module.css";
+
+export default function About(){
+    return(
+        <div className={styles.about}>
+            <h1>This is About Page.</h1>
+        </div>
+    )
+} 
